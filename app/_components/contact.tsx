0f84c3a8ff86bd6814cc2b7f7e6afff32a0bb080
rb-sgv <rb@spyglassventures.ch>
2026-08-11 15:40:19 +0200

    feat: added contact page

---
+++ app/_components/contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='bg-[#ff9f1c] rounded-lg m-1 flex justify-center w-fit ml-auto mr-auto mt-20 text-[#CBF3F0] text-center'>
+        <ul className='p-5'>
+            <li className='mt-1 mb-5 text-3xl'>Contact me!</li>
+            <li>John Doe</li>
+            <li>name@example.com</li>
+            <li>+1 (555) 123-4567</li>
+            <li>123 Example St, City, State 12345</li>
+        </ul>
+        </div>
+  )
+}
+
+export default Contact         
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from "../_components/contact";
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <>
+        <Header />
+        <Contact />
+    </>
+  );
+}
