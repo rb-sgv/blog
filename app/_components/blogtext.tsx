cca05c7006345431a6b729a7e538664ed07a7c0b
rb-sgv <rb@spyglassventures.ch>
2026-08-11 08:30:08 +0200

    fix: trying a different approach for the blog post stuff

--- app/_components/blogtext.tsx
+++ app/_components/blogtext.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { post } from '../_pages/pages'
 
-const Blogtext = () => {
-    return (
-      <div className=" bg-[#ff9f1c] rounded-lg m-1 flex justify-center w-fit ml-auto mr-auto mt-20">
-        {post.map((post) => (
-          <article key={post.slug}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </article>
-        ))}
-      </div>
-    )
-  }
-
-export default Blogtext
-  
-  
-  
+export default function blogtext() {
+  return (
+    <div className='bg-[#ff9f1c]'>
+      <h1 className='text-[#CBF3F0] hover:text-[#2EC4B6]'>
+        Posts
+      </h1>
+    </div>
+  )
+}
