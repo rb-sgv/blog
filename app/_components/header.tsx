140584b45d544156cb31f31dd61b83c311c183cb
rb-sgv <rb@spyglassventures.ch>
2026-08-13 14:41:04 +0200

    style(navbar): made the hover text on the navbar smaller, removed the latblog text, centered the rest

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,16 +4,13 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <nav className='bg-[#fe9100]/20 backdrop-blur-md text-[#CBF3F0]'>
-        <div className='max-w-6xl mx-auto px-6 py-4 flex justify-between items-center'>
-          <Link href="/" className='text-xl hover:text-[#2EC4B6] hover:scale-165 transition delay-0'>
-            Lat-Blog
-          </Link>
+        <div className='max-w-6xl mx-auto px-6 py-4 flex justify-center items-center'>
 
           <div className='hidden md:flex space-x-6'>
-            <Link href="/" className='hover:text-[#2EC4B6] hover:scale-165 transition delay-0'>Home</Link>
-            <Link href="/about" className='hover:text-[#2EC4B6] hover:scale-165 transition delay-0'>About</Link>
-            <Link href="/posts" className='hover:text-[#2EC4B6] hover:scale-165 transition delay-0'>Posts</Link>
-            <Link href="/contact" className='hover:text-[#2EC4B6] hover:scale-165 transition delay-0'>Contact</Link>
+            <Link href="/" className='hover:text-[#2EC4B6] hover:scale-105 transition delay-0'>Home</Link>
+            <Link href="/about" className='hover:text-[#2EC4B6] hover:scale-105 transition delay-0'>About</Link>
+            <Link href="/posts" className='hover:text-[#2EC4B6] hover:scale-105 transition delay-0'>Posts</Link>
+            <Link href="/contact" className='hover:text-[#2EC4B6] hover:scale-105 transition delay-0'>Contact</Link>
           </div>
 
         </div>
